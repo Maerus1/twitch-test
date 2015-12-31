1cd53b1b00637062330ac873d5b2e8dde82347fc
Dan Dorsey <Maerus1@users.noreply.github.com>
2015-12-31 11:56:55 -0500

    Added text fields
    I added a couple text fields, one to search the list for a streamer, the
    other to add a new streamer to the list. Now I need to optimize the
    search function and find a way to save the users list

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(function(){
   var users = ["freecodecamp", "lowkotv", "juliesss", "koshkiisc", "medrybw", "missmagitek", "nathanias","kanesc2","majinphil","polt", "raggen71"];
+  var used = []; //this helps prevent the creation of duplicates
   var text = $("#text");
 
   for(var i in users){
@@ -12,9 +13,7 @@ $(function(){
       "<p style='display:inline'>" + obj.display_name + "</p><br/><br/>" +
       "</div>" +
       "</a>"
-    ); //create elements to be contained within a div containing the correct info from the twitch API
-  //  console.log(names[i]); //learn about MIME checking
-  console.log(obj);
+    ); //learn about MIME checking
 });
 $.getJSON('https://api.twitch.tv/kraken/streams/' + users[i], function(obj){
   if(obj.stream !== null){ //highlight if online (green)
@@ -23,4 +22,45 @@ $.getJSON('https://api.twitch.tv/kraken/streams/' + users[i], function(obj){
 });
 })(i); //this (function(i){})(i); prevents the for loop above it from going straight to the end when it goes through the callback function
 }
+
+  var box = $("#box");
+  var texts = $("#texts");
+  $(box).keyup(function(){
+    $(texts).text($(this).val());
+    if($(this).val() !== ''){
+      $("p:not(:contains(" + $(this).val() + "))").closest("div").hide();
+    }
+    else {
+      $("p").closest("div").show();
+    }
+  });
+
+  //add new streamer to list and re-populate
+  var add = $("#add");
+  add.keypress(function(){
+    if(event.which === 13){
+      users.push(add.val());
+      $.getJSON("https://api.twitch.tv/kraken/channels/" + add.val(), function(obj){
+        text.append(
+          "<a href='" + obj.url + "' target='_blank'>" +
+          "<div class='name" + users.length.toString() + "'>" +
+          "<img src='" + obj.logo + "'/>" +
+          "<p style='display:inline'>" + obj.display_name + "</p><br/><br/>" +
+          "</div>" +
+          "</a>"
+        );
+    }).error(function(){
+      console.log("File not found!");
+    });
+
+    $.getJSON('https://api.twitch.tv/kraken/streams/' + add.val(), function(obj){
+      if(obj.stream !== null){ //highlight if online (green)
+        $(".name" + users.length.toString()).css("background-color", "green");
+      }
+
+    });
+    }
+  });
+
+
 });
